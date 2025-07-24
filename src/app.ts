572a81ba9a43c549f47cb1692e690dd5a8959b29
AndreeG29 <ogonzalezc11@miumg.edu.gt>
2025-07-23 22:41:59 -0600

    ruta de prueba

--- src/app.ts
+++ src/app.ts
@@ -30,3 +30,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
+
+app.get('/', (_req, res) => {
+  res.send('✅ API corriendo correctamente en Render');
+});
